95b61bd203743c573b8f191ef20811e27ad37cde
Jaedson Silva <contacanal798@gmail.com>
2023-04-18 14:47:13 -0300

    feat: add config and hash data file paths

--- openshield/scanner.py
+++ openshield/scanner.py
@@ -1,1 +1,8 @@
+import os
+from pathlib import Path
+
+HOME_USER = Path.home()
+CONFIG_FILEPATH = os.path.join(HOME_USER, 'openshield.ini')
+HASH_DATA_PATH = os.path.join(HOME_USER, 'hashes.openshield.zip')
+
 MD5_HASH_DOWNLOAD_URL = 'https://bazaar.abuse.ch/export/txt/md5/full/'
