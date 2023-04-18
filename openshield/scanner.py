be0ed4cd29f2e6322f2e9d8825cbcddfa7186adc
Jaedson Silva <contacanal798@gmail.com>
2023-04-18 15:28:47 -0300

    feat: create config file if not exists

--- openshield/scanner.py
+++ openshield/scanner.py
@@ -15,6 +15,12 @@ class Scanner:
         self._config = ConfigParser()
         self._config.read(CONFIG_FILEPATH)
 
+        if not self._config['DEFAULT']:
+            self._config['DEFAULT']['lastUpdate'] = '2000-01-01 00:00:00'
+            
+            with open(CONFIG_FILEPATH, 'w') as _config:
+                self._config.write(_config)
+
     def require_hashes_update(self) -> bool:
         config = self._config['DEFAULT']
         last_update = datetime.strptime(config['lastUpdate'], '%Y-%m-%d, %H:%M:%S')
