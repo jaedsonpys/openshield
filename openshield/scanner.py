90a91d9b18edf91a2cb5280d3496fa9d75ff31ed
Jaedson Silva <contacanal798@gmail.com>
2023-04-18 15:09:26 -0300

    feat: add method to check if a new hashes update is required

--- openshield/scanner.py
+++ openshield/scanner.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from configparser import ConfigParser
+from datetime import datetime, timedelta
 
 HOME_USER = Path.home()
 CONFIG_FILEPATH = os.path.join(HOME_USER, 'openshield.ini')
@@ -13,3 +14,8 @@ class Scanner:
     def __init__(self) -> None:
         self._config = ConfigParser()
         self._config.read(CONFIG_FILEPATH)
+
+    def require_hashes_update(self) -> bool:
+        config = self._config['DEFAULT']
+        last_update = datetime.strptime(config['lastUpdate'], '%Y-%m-%d, %H:%M:%S')
+        return datetime.now() >= (last_update + timedelta(hours=1))
