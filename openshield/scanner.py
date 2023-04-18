45230d7109e344b631eac4e43a162aba61561956
Jaedson Silva <contacanal798@gmail.com>
2023-04-18 14:52:58 -0300

    feat: create Scanner() class and read config file in __init__

--- openshield/scanner.py
+++ openshield/scanner.py
@@ -1,8 +1,15 @@
 import os
 from pathlib import Path
+from configparser import ConfigParser
 
 HOME_USER = Path.home()
 CONFIG_FILEPATH = os.path.join(HOME_USER, 'openshield.ini')
 HASH_DATA_PATH = os.path.join(HOME_USER, 'hashes.openshield.zip')
 
 MD5_HASH_DOWNLOAD_URL = 'https://bazaar.abuse.ch/export/txt/md5/full/'
+
+
+class Scanner:
+    def __init__(self) -> None:
+        self._config = ConfigParser()
+        self._config.read(CONFIG_FILEPATH)
